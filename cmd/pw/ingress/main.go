fstachura/patchwork-next

commit 82a9d25f22e3f42c80bce99c30c12b4e4d5c6e86

ingress: pass context with event bus to SMTP mail parser
The SMTP session handler calls ParseMail with context.Background()
which does not carry the EventBus. When Commit() tries to enqueue
pending events, it dereferences a nil pointer and panics. The --stdin
and --mbox paths already use the proper context.

Store the context (with the bus) in the backend struct and pass it
through to ParseMail.

Fixes: bc70c3796738 ("events: add event bus")
Signed-off-by: Robin Jarry <robin@jarry.cc>

cmd/pw/ingress/main.go
@@ -82,6 +82,7 @@ func (c *CLI) Run(ctx *pw.Context) error {
 	}
 
 	be := &backend{
+		ctx:      ctx.Context,
 		database: ctx.DB,
 		cfg:      ctx.Config,
 		listID:   c.ListID,
@@ -141,6 +142,7 @@ func startSMTPServer(cfg *config.Config, be smtp.Backend) (net.Listener, *smtp.S
 }
 
 type backend struct {
+	ctx      context.Context
 	database *bun.DB
 	cfg      *config.Config
 	listID   string
@@ -223,7 +225,7 @@ func (s *session) Data(r io.Reader) error {
 		entity.Header.Get("Subject"))
 
 	err = mail.ParseMail(
-		context.Background(), s.backend.database,
+		s.backend.ctx, s.backend.database,
 		bytes.NewReader(data), s.backend.listID,
 	)
 	if err != nil {

commit 469071b06d8df3c403b82c54683e8319e2f67892

ingress: fix crash when parsing from stdin
When injecting a single email or an mbox from stdin, ParseMail is
invoked with the wrong context object which does not have a reference to
the event bus.

This results in a NULL pointer dereference on Queries.Commit():

	for _, e := range q.pendingEvents {
		q.Events.Enqueue(&e) // q.Events is nil
	}

Use the correct context object to fix the issue.

Add a panic() to ensure that transactions can never be started with
a nil event bus anymore.

Fixes: 82a9d25f22e3 ("ingress: pass context with event bus to SMTP mail parser")
Reported-by: Thomas Bracht Laumann Jespersen <t@laumann.xyz>
Signed-off-by: Robin Jarry <robin@jarry.cc>

cmd/pw/ingress/main.go
@@ -63,7 +63,7 @@ func (c *CLI) Run(ctx *pw.Context) error {
 				if err != nil {
 					break
 				}
-				err = mail.ParseMail(ctx, ctx.DB, msg, c.ListID)
+				err = mail.ParseMail(ctx.Context, ctx.DB, msg, c.ListID)
 				if errors.As(err, &dupErr) {
 					log.Debugf("ignoring %s", err)
 				} else if err != nil {
@@ -71,7 +71,7 @@ func (c *CLI) Run(ctx *pw.Context) error {
 				}
 			}
 		} else {
-			err = mail.ParseMail(ctx, ctx.DB, os.Stdin, c.ListID)
+			err = mail.ParseMail(ctx.Context, ctx.DB, os.Stdin, c.ListID)
 		}
 		if errors.As(err, &dupErr) {
 			log.Debugf("ignoring %s", err)

pkg/db/db.go
@@ -124,10 +124,11 @@ func WithBus(ctx context.Context, bus EventBus) context.Context {
 }
 
 func GetBus(ctx context.Context) EventBus {
-	if bus, ok := ctx.Value(busCtxKey{}).(EventBus); ok {
-		return bus
+	bus := ctx.Value(busCtxKey{}).(EventBus)
+	if bus == nil {
+		panic("Queries.Begin() context has no event bus value")
 	}
-	return nil
+	return bus
 }
 
 // Queries provides typed database access methods. It wraps a bun.IDB